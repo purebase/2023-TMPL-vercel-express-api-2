4a6ad2c0297c86f24a14031f32657f6d7cd2f505
christopheilermann <user@purebase.de>
2023-10-25 00:23:07 +0200

    Moved js into src folder

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
+// wir genommen!
 app.use("/home", home);
 
 // connection
--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
+    title: "Express Testing with src",
     message: "The app is working properly!",
   });
 });
